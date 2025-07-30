58933c8323340f344687da786e1533c89d35c1ec
renzhenming <1565766177@qq.com>
2025-07-30 21:23:50 +0800

    第一个着色器

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,75 +1,80 @@
 #include "ofApp.h"
 ofMesh triangle;
-//--------------------------------------------------------------
-void ofApp::setup(){
+ofShader shader;
 
-	triangle.addVertex(glm::vec3(0.0, 0.0, 0.0));
-	triangle.addVertex(glm::vec3(0.0, 768.0f, 0.0));
-	triangle.addVertex(glm::vec3(1024.0f, 768.0f, 0.0));
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	triangle.addVertex(glm::vec3(-1.0, -1.0, 0.0));
+	triangle.addVertex(glm::vec3(-1.0, 1.0, 0.0));
+	triangle.addVertex(glm::vec3(1.0, 1.0, 0.0));
+	bool loaded = shader.load("shader.vert", "shader.frag");
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
+	shader.begin();
 	triangle.draw();
+	shader.end();
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
 	glm::vec3 curPos = triangle.getVertex(2);
 	triangle.setVertex(2, curPos + glm::vec3(0, 20, 0));
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
